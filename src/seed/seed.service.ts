c341d659542c7fdf8d5d602ece384da9ce666f0c
tauesp127-alexis <gestionti@taurus.com.mx>
2024-11-21 13:17:40 -0600

    Product SEED, importar servicio dentro de un modulo

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { ProductsService } from 'src/products/products.service';
+
+
+@Injectable()
+export class SeedService {
+
+  constructor( private readonly productsService: ProductsService ) {}
+  
+  async runSeed() {
+    await this.insertNewProducts();
+    return 'SEED executed'
+  }
+
+  private async insertNewProducts() {
+    this.productsService.deleteAllProducts();
+    return true;
+  }
+
+}

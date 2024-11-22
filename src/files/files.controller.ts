0bdc5158bedbe16128118d8d697f6ac1ea8774b7
tauesp127-alexis <gestionti@taurus.com.mx>
2024-11-22 10:36:07 -0600

    Retornar el SecureUrl

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -4,11 +4,15 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { fileFilter, fileNamer } from './helpers';
 import { diskStorage } from 'multer';
 import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService
+  ) {}
 
   @Get('product/:imageName')
   findProductImage(
@@ -36,7 +40,7 @@ export class FilesController {
       throw new BadRequestException('Make sure the file is an image');
     }
 
-    const secureUrl = `${ file.filename }`
+    const secureUrl = `${ this.configService.get('HOST_API') }/files/product/${ file.filename }`;
 
     return {
       secureUrl
